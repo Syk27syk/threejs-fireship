ac55d5c413f85b74416e465ef28629d26567e196
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-16 22:04:14 +0800

    orbit controls

--- threejs-fireship/main.js
+++ threejs-fireship/main.js
@@ -44,6 +44,7 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper)
 
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // animate
 function animate() {
@@ -53,6 +54,8 @@ function animate() {
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
 
+    controls.update();
+    
     renderer.render( scene, camera );
 
 }
