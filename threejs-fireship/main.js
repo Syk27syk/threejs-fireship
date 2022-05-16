7b28da49aa202325dd77c4f9315be1e03eb5bfc1
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-16 18:51:32 +0800

    create shape and animate

--- threejs-fireship/main.js
+++ threejs-fireship/main.js
@@ -1,6 +1,9 @@
 import './style.css'
+
+// import threejs
 import * as THREE from 'three';
 
+// scene, camera, and renderer
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -14,3 +17,23 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
 renderer.render ( scene, camera );
+
+// add our shape(s)
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+const torus = new THREE.Mesh( geometry, material);
+
+// render
+scene.add(torus)
+
+    //renderer.render( scene, camera);
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+
+}
+
+animate()
+
